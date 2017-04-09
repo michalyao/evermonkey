4ea04d1bc156c3a7f06d2c7b977d05758d07a808
Yao <yaoyao0777@gmail.com>
2017-04-09 18:12:23 +0800

    add  file exists function

--- src/file.ts
+++ src/file.ts
@@ -4,3 +4,9 @@ import * as bluebird from 'bluebird';
 const fs = bluebird.Promise.promisifyAll(fsn);
 
 export default fs;
+
+fs.exsit = function(path) {
+   return new bluebird.Promise((resolve) => {
+    fsn.access(path, fsn.constants.F_OK, err => resolve(!err))
+  });
+}
